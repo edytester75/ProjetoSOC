1c720db19e40698a51703b99dc8ae6320d9f457b
Edivaldo Luiz de Faria <edivaldo.faria@dorconsultoria.com.br>
2022-09-27 13:31:33 -0300

    Atualizar comando

--- src/test/java/runner/ExecuteTest.java
+++ src/test/java/runner/ExecuteTest.java
@@ -30,6 +30,6 @@ public class ExecuteTest {
     @AfterClass
     public static void tearDown(){
         System.out.println("Finalizou ");
-        driver.quit();
+       // driver.quit();
     }
 }
